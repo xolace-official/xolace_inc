e9d39d0c9e8171cc7370fb45b565a408c63f9f7b
Beniako Andrew Nana <b.qhuabenah17@gmail.com>
2025-06-04 16:26:41 +0000

    update
    moved the footer component from page to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "../components/Navigation/Navbar";
+import { Footer } from "@/components/Footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );

119b2106c331a9e43b63e44a2ee62daf184ea964
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-06-16 21:11:34 +0000

    Update page.tsx

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/text-area";
